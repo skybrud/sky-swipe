d44eb416e6bd71bb3175dab5845aa27beed17559
Mathias Helmuth Pedersen <mhelmuth@skybrud.dk>
2016-05-13 13:35:13 +0200

    Remove skyShow from module

--- sky-swipe.module.ts
+++ sky-swipe.module.ts
@@ -2,7 +2,7 @@
 (function () {
 	'use strict';
 
-	angular.module('skySwipe',['skyShow','layout']);
+	angular.module('skySwipe', []);
 
 	//BEWARE: this requires the thirdparty plugin https://github.com/thebird/Swipe
 })();

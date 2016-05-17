780ab9cfb6a2d5f283a93ccdbbf4e573dde04ec5
Mathias Helmuth Pedersen <mhelmuth@skybrud.dk>
2016-05-17 10:49:30 +0200

    Added custom autoplay (with delay property)

--- readme.md
+++ readme.md
@@ -3,10 +3,14 @@
 Angular-module wrapper for swipe.js.
 https://github.com/thebird/Swipe
 
-Please refer to the code-files for usage and documentation!
+The `sky-swipe` directive has some optional attributes:
+- `auto`: enables autoplay
+- `delay`: sets an initial delay before autoplaying (useful if multiple swipes are on the same page, and you would like to avoid synchronous playback). Both attributes are defined in milliseconds.
+
+Please refer to the code-files for more documation and usage specifics.
 
 ```html
-<sky-swipe>
+<sky-swipe auto="5000" delay="500">
 	<div><img src="http://lorempixel.com/500/280/sports/1/" alt="" /></div>
 	<div><img src="http://lorempixel.com/500/280/sports/2/" alt="" /></div>
 	<div><img src="http://lorempixel.com/500/280/sports/3/" alt="" /></div>
--- sky-swipe.directive.ts
+++ sky-swipe.directive.ts
@@ -9,7 +9,6 @@ declare var Swipe: any;
 	 * Directive for our integration of the `swipe.js` gallery
 	 * (which is a manual dependency)
 	 *
-	 * Uses skyShow to show either bullets or controls based on touch-capabilities.
 	 *
 	 * This gallery relies on styling from swipe.scss
 	 *
@@ -28,11 +27,13 @@ declare var Swipe: any;
 			link:link
 		};
 		function link(scope,element,attributes) {
-			element.addClass('swipe-gallery');
-			scope.images=[];
-			
-			scope.touch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+			var intervalTimer = 0;
+			var canPlay = true;
 
+			scope.images=[];
+			scope.touch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+			scope.autoplay = (attributes.hasOwnProperty('auto') && attributes.auto > 0 && !scope.touch);
+				
 			/* Build scope.images from the used elements */
 			angular.forEach(element.find('div'),function(element,key) {
 				scope.images.push({
@@ -44,10 +45,12 @@ declare var Swipe: any;
 			scope.images[0].active=true;
 			scope.current=1;
 
+			element.addClass('swipe-gallery');
+
 			/* Defining the gallery, and the callback method that runs on each swipe */
 			var gallery = new Swipe(element[0], {
-				continuous: false,
-				callback:function(which) {
+				continuous: attributes.hasOwnProperty('continuous') || scope.autoplay,
+				callback: function(which) {
 					/**
 					 * Whenever the image is changed (swiped or via controls), update
 					 * `scope.current`, and set the `active` property on the specific
@@ -57,34 +60,77 @@ declare var Swipe: any;
 					 *
 					 **/
 					$timeout(function() {
-						angular.forEach(scope.images, function(item,key) {
+						angular.forEach(scope.images, function(item, key) {
 							if (which == key) {
 								scope.images[key] = {
-									active:true
+									active: true
 								};
 							} else {
 								scope.images[key] = {
-									active:false
+									active: false
 								};
 							}
 						});
 
-						scope.current=which+1;
+						scope.current = which + 1;
 
-					},0);
+					}, 0);
 				}
 			});
+
+			/* setting up custom autoplay with pause on mouse hover, touch etc. */
+			if (scope.autoplay) {
+				element[0].addEventListener('mouseenter', userActive);
+				element[0].addEventListener('mouseleave', userInactive);
+				element[0].addEventListener('touchstart', userActive);
+				element[0].addEventListener('touchend', userInactive);
+
+				setTimeout(() => {
+					playInterval();
+				}, attributes.delay || 0);
+			}
+
 
 			/* Assigning swipe.js' slide-, next- and prevmethods to the scope */
 			scope.slide=function(which) {
 				gallery.slide(which);
+				resetInterval();
 			};
 			scope.next=function() {
 				gallery.next();
+				resetInterval();
 			};
 			scope.prev=function() {
 				gallery.prev();
+				resetInterval();
 			};
+
+
+			/* Autoplay methods */
+			function playInterval() {
+				if (canPlay && scope.autoplay) {
+					intervalTimer = setInterval(gallery.next, attributes.auto);
+				}
+			}
+
+			function pauseInterval() {
+					clearInterval(intervalTimer);
+			}
+
+			function resetInterval() {
+				pauseInterval();
+				playInterval();
+			}
+
+			function userActive() {
+				canPlay = false;
+				pauseInterval();
+			}
+
+			function userInactive() {
+				canPlay = true;
+				playInterval();
+			}
 		}
 
 		return directive;
